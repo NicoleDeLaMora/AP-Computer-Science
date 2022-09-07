9558fd94e3110905aacc9c33f514247e7b762016
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2022-09-07 10:36:53 -0500

    Update

---
+++ src/Prog52a.java
@@ -0,0 +1,18 @@
+public class Prog52a {
+    public static void main(String[] args) {
+        int length = 143;
+        int width = 82;
+       //If you want to change width later, just do width = 90;
+
+        //Basic math signs:  + - * / %
+        int area = length * width;
+        int perimeter = (2 * width)+ (2 * length);
+
+        System.out.println("Area = " + area);
+        System.out.println("Perimeter =" + perimeter);
+    }
+}
+/*
+Area = 11726
+Perimeter =450
+ */
