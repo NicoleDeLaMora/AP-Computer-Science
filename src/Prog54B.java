c91038c2088cd40df7112c5e2fdbf66fe243119e
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2022-09-09 10:22:13 -0500

    Update

---
+++ src/Prog54B.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Prog54B {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is the first number?");
+        int A = scan.nextInt();
+        System.out.println("What is the second number?");
+        int B = scan.nextInt();
+        System.out.println("What is the third number?");
+        int C = scan.nextInt();
+        System.out.println("What is the fourth number?");
+        int D = scan.nextInt();
+        int num1 = A + B + C + D;
+
+
+    }
+
+}
---
+++ src/Prog58B.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Prog58B {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // INPUTS
+        System.out.print("What is A? ");
+        double A = scan.nextDouble();
+
+        System.out.print("What is B? ");
+        double B = scan.nextDouble();
+
+        System.out.print("What is C?");
+        double C = scan.nextDouble();
+
+        Double cube = Math.pow(A, 2) ;
+        Double calc = cube
+    }
+
+
+
+}
