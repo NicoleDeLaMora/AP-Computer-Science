4736a9414a9b88a9d11d07dd07fd4d77a3925f35
NicoleDeLaMora <112962823+NicoleDeLaMora@users.noreply.github.com>
2022-09-10 20:58:08 +0000

    Completed Program 58B

--- src/Prog58B.java
+++ src/Prog58B.java
@@ -14,8 +14,11 @@ public class Prog58B {
         System.out.print("What is C?");
         double C = scan.nextDouble();
 
-        Double cube = Math.pow(A, 2) ;
-        Double calc = cube
+        Double square = Math.pow(A, 2);
+      Double rootA = (-B+ Math.sqrt(Math.pow(B,2) - 4*A*C) )/2*A;
+    Double rootB = (-B - Math.sqrt(Math.pow(B,2) - 4*A*C) )/2*A;
+    System.out.println("The roots are: " + rootA + " and " + rootB );
+    
     }
 
 
