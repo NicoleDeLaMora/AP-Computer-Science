e7de2a15ffd41836cc6730d9d9dcf5ab7541a762
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2022-09-19 09:58:29 -0500

    115a

---
+++ src/Prog115a.java
@@ -0,0 +1,30 @@
+//Nicole de la Mora // 9/19/22 // prog 115a
+public class Prog115a {
+public static void main(String[] args){
+    int lcv = 0; //LCV: loop control varient
+    while (lcv < 36){
+        lcv += 2; // counting all even numbers
+        System.out.println(lcv);
+    }
+}
+}
+/*
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+22
+24
+26
+28
+30
+32
+34
+36
+ */
