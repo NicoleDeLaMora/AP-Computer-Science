b16ce58c3306696a5302d08370cb6f11c01de4dd
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2022-09-20 09:59:21 -0500

    115a, 122a, 122b, 122c

--- src/Prog122c.java
+++ src/Prog122c.java
@@ -4,15 +4,22 @@ public class Prog122c {
         int num = 0;
         int add = 0;
         int deez = 0;
-        int sq = 0;
+        double sq = 0;
 
-        while(lcv < 6) {
+        while(lcv < 5) {
+            lcv += 1;
             num = lcv * 2;
             add = num+1;
             deez = num * 2;
             sq = Math.pow(num, 2);
-
-
+            System.out.println(num +"    " + add +"    "+deez + "    " +sq);
         }
     }
 }
+/*
+2    3    4    4.0
+4    5    8    16.0
+6    7    12    36.0
+8    9    16    64.0
+10    11    20    100.0
+ */
