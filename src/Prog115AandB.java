8ee1bc0e8216c117bdf471279fcf8f1a7bfbf305
NicoleDeLaMora <112962823+NicoleDeLaMora@users.noreply.github.com>
2022-09-20 11:32:34 -0500

    Update and rename Prog115a.java to Prog115AandB.java
    Tried 115B

--- src/Prog115AandB.java
+++ src/Prog115AandB.java
@@ -1,11 +1,16 @@
 //Nicole de la Mora // 9/19/22 // Program 115a
 public class Prog115a {
 public static void main(String[] args){
-    int lcv = 0; //LCV: loop control varient
+    int lcv1 = 0; // one for horizontal 
+int lcv = 0; //LCV: loop control varient
     while (lcv < 36){
         lcv += 2; // counting all even numbers
         System.out.println(lcv);
     }
+while (lcv1 < 36){
+lcv +=2
+System.out.println(lcv1);
+} 
 }
 }
 /*
