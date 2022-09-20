8c4bfd5868603441b34f36c1fa597ec6ef0af189
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2022-09-20 10:31:08 -0500

    115a, 122a, 122b, 122c

---
+++ src/Prog122deez.java
@@ -0,0 +1,13 @@
+//Nicole de la Mora // 9/20/22 // Program 122D(eez nuts)
+public class Prog122deez {
+    public static void main(String[] args) {
+        System.out.println("x               y");
+        int lcv = 0;
+
+        while ( lcv < 28 ){
+            int x = -12;
+            double y = Math.pow(x, 6) - (3*Math.pow(x,5)) - (93 * (x,4) + (87 * Math.pow(x, 3)) + (1596 * Math.pow(x,2)) - (1380 * x) -2800;
+            lcv+=1;
+        }
+    }
+}
