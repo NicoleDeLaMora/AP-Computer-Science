20586ab0dd213ee0166dbc4f7e441b4bc94be150
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2022-09-26 10:28:42 -0500

    worked on lp

---
+++ src/LP41.java
@@ -0,0 +1,11 @@
+//Nicole de la Mora // 9/26/22 // LP 4-1
+import java.util.*;
+
+public class LP41 {
+    Scanner scan = new Scanner(System.in);
+    double copi = 0;
+    double dolla = 0;
+    double tot = 0;
+
+
+}
