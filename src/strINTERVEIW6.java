c8e5b62635e9e1d61f42a876aaa04d745e1e6568
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2022-09-28 10:47:32 -0500

    intewrveiw7 did and done interveiw 6 sstruggled

---
+++ src/strINTERVEIW6.java
@@ -0,0 +1,29 @@
+//Nicole de la Mora // 9/28/22 // String Interveiw 6
+import java.util.*;
+public class strINTERVEIW6 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a string!");
+        String text = scan.nextLine();
+
+        text = text.toLowerCase();
+        int acount = 0; int bcount = 0; int ccount = 0; int dcount = 0; int ecount = 0; int fcount = 0; int gcount = 0; int hcount = 0; int icount = 0; int jcount = 0; int kcount = 0; int lcount = 0; int mcount = 0; int ncount = 0; int ocount = 0; int pcount = 0; int qcount = 0; int rcount = 0; int scount = 0; int tcount = 0; int ucount = 0; int vcount = 0; int wcount = 0; int xcount = 0; int ycount = 0; int zcount = 0;
+
+        for (int lcv = 0; lcv<text.length(); lcv++){
+            char cur = text.charAt(lcv);
+            if (cur == 'a'){
+                acount++;
+
+
+
+            }
+
+
+
+
+        }
+    }
+}
+/*
+
+ */
