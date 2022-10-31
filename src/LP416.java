2b83a971fa958ef2970259ad5b0824b9df0ea9a8
Nicole <112962823+NicoleDeLaMora@users.noreply.github.com>
2022-10-31 14:45:07 +0000

    cried

---
+++ src/LP416.java
@@ -0,0 +1,10 @@
+//Nicole de la Mora // 10/30 // LP 4-16
+import java.util.*;
+public class LP416{
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter an angle in degrees: ");
+      int angle = scan.nextInt();
+    
+  }
+}
--- src/LP59.java
+++ src/LP59.java
@@ -1,20 +1,24 @@
+
 //Nicole de la Mora // LP5-9
 import java.util.*;
-public class LP59{
-  public static void main(String[] args){
-    System.out.println("x^1 \t x^2 \t x^3 \t x^4 \t x^5")
-int lcv = 0;
-    while (lcv < 7){
 
-      int p1 = math.pow(lcv,1);
-      int p2 = math.pow(lcv,2);
-      int p3 = math.pow(lcv,3);
-      int p4 = math.pow(lcv,4);
-      int p5 = math.pow(lcv,5);
-      System.out.printf(""%d/t/t%d/t/t%d/t/t%d/t/t%d/t/t", (int)p1, (int)p2, (int)p3, (int)p4, (int)p5);
+public class LP59 {
+  public static void main(String[] args) {
+    System.out.println("x^1 \t x^2 \t x^3 \t x^4 \t x^5");
+    int lcv = 1;
+    while (lcv < 7) {
+
+      double p1 = Math.pow(lcv, 1);
+      double p2 = Math.pow(lcv, 2);
+      double p3 = Math.pow(lcv, 3);
+      double p4 = Math.pow(lcv, 4);
+      double p5 = Math.pow(lcv, 5);
+      System.out.println(p1 + "      " + p2 + "      " + p3 + "      " + p4 + "      " + p5);
+      lcv++;
     }
-  
-    //         System.out.printf("%d\t\t%d\t\t%.5f\t\t%d\t\t%.5f\n",(int)num,(int)square,sqrt,(int)cube,root);
+    System.out.printf("%.5\t\t%.5f\t\t%.5f\t\t%.5f\t\t%.5f\n", (int)p1, p2, p3, p4, p5);
+
+    // System.out.printf("%d\t\t%d\t\t%.5f\t\t%d\t\t%.5f\n",(int)num,(int)square,sqrt,(int)cube,root);
 
   }
 }
