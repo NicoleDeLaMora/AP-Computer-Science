22d4b75fdfa44f94868a9028d7fce2e364c9aac7
Nicole <112962823+NicoleDeLaMora@users.noreply.github.com>
2022-11-03 19:33:23 +0000

    woeked on 88a class

--- src/Prog88aCl.java
+++ src/Prog88aCl.java
@@ -1,4 +1,4 @@
-//Nicole de la Mora // 11/3//22 // Prog 88a Class
+t//Nicole de la Mora // 11/3//22 // Prog 88a Class
 public class Prog88aCl {
   private int sum;
   private int dif;
@@ -20,6 +20,21 @@ public class Prog88aCl {
     max = 0;
 
   }
+  public void calcIT(){
+    sum = (num1+num2);
+    dif = Math.abs(num1-num2);
+    prod = num1*num2;
+    ave = (num1+num2)/2;
+    dis = Math.abs(num1 - num2) + num1 + num2;
+    if (num1>num2){
+      max = num1;
+    }else max = num2;
+  }
+  public int getSum(){return sum;}
+  public int getDif(){return dif;}
+  public double getProd(){return prod;}
+  public double getAve(){return ave;}
+  public int getDis(){return dis;}
+  public int getMax(){return max;}
   
-
-}
+  }
--- src/Prog88aClass.java
+++ src/Prog88aClass.java
@@ -1,7 +1,26 @@
+
 //Nicole de la Mora // 11/3//22 // Prog 88a Class
-//Driver 
+//Driver
+import java.util.*;
+
 public class Prog88aClass {
   public static void main(String[] args) {
+  Scanner scan = new Scanner(System.in);
+    System.out.print("Enter a number 1-13:");
+    int a = scan.nextInt();
+    System.out.print("Enter a number 2-20: ");
+    int b = scan.nextInt();
 
+    Prog88aClass wow = new Prog88aClass;
+    wow.calcIT();
+
+    int sum = wow.getSum();
+    int dif = wow.getDif();
+    int Prod = wow,getProd();
+    double ave = wow.getAver();
+    int dis = wow.getDis();
+    int max = wow.getMax();
+    
+    
   }
 }
