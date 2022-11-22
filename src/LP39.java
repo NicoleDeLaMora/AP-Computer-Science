a388fbe4f0ce658890fa2458353b39556e1fb7e0
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2022-11-22 10:20:00 -0600

    LP3-9 struggle for some stupid reason

---
+++ src/LP39.java
@@ -0,0 +1,24 @@
+//Nicole de la Mora // 11/22/22 // LP 3-9
+import java.util.*;
+public class LP39 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your Birthdate: ");
+        System.out.println("Year: ");
+        System.out.println("Month: ");
+        System.out.println("Day: ");
+        System.out.println("Enter todays date: ");
+        System.out.println("Year: ");
+        System.out.println("Month: ");
+        System.out.println("Day: ");
+
+        int birthy = scan.nextInt();
+        int birthm = scan.nextInt();
+        int birthd = scan.nextInt();
+        int tdayy = scan.nextInt();
+        int tdaym = scan.nextInt();
+        int tdayd = scan.nextInt();
+
+
+    }
+}
