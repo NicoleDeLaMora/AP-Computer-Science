f3b41e13fc8f47afdbb65d9e526e8cf0319ffefd
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2022-11-23 10:44:21 -0600

    ughhhhh

---
+++ src/MSOE1.java
@@ -0,0 +1,13 @@
+//Nicole de la Mora // 11/23/22 // MSOE-2008-1
+import java.util.*;
+public class MSOE1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is the string? ");
+        String str = scan.nextLine();
+        if(str.substring(0,1).equals(str.substring(str.length()-1, str.length()))){
+
+        }
+        for(int lxv = 0)
+    }
+}
