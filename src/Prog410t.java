5159003bb533fe9a87a4fbfe7d9a2ed1030e12d1
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2022-12-05 10:28:46 -0600

    410t

---
+++ src/Prog410t.java
@@ -0,0 +1,14 @@
+//nicole de la Mora // 12/5/22 // Prog410t
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+public class Prog410t {
+    public static void main(String[] args){
+        try {  // On Replit, may need to put "../data/prog285b.dat"
+            Scanner input = new Scanner(new File("data/survey.dat"));
+            ArrayList<>
+        } catch (IOException e) {
+            System.out.println("Can't find data file!");
+        }
+    }
+}
