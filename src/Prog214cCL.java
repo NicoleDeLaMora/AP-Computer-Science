c11b57cdee23b1d5ae8b2bebb08a6c46c002457c
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2022-12-07 10:47:07 -0600

    help

---
+++ src/Prog214cCL.java
@@ -0,0 +1,7 @@
+public class Prog214cCL {
+    private String type;
+    private String Wash;
+    private double galCost;
+    private double Cost;
+    private double
+}
---
+++ src/Prog241c.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Prog241c {
+    public static  void main(String[] args){
+    /*
+    A service station with a car wash sells three grades of gasoline:
+    regular unleaded at $1.359 per gallon,
+    premium at $1.479 per gallon,
+    and high octane at $1.429 per gallon
+
+    A car wash normally costs $2.00, but the station offers a 10% reduction on the price of a
+    wash for every full gallon of gasoline purchased in the excess of the first 10 gallons.
+    When 20 or more gallons are purchased, the car wash is free
+
+
+      The program should print the cost of the gasoline and the price of a wash,
+      read another character indicates whether or not the customer wants a car wash,
+      and then print the total charges in the form of an invoice
+
+     */
+        try {  // On Replit, may need to put "../data/prog285b.dat"
+            Scanner input = new Scanner(new File("data/prog214c.dat"));
+
+
+        } catch (IOException e) {
+            System.out.println("Can't find data file!");
+        }
+
+    }
+}
