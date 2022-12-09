caaf9933d6ce4130b388adc589f0717771559309
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2022-12-09 10:28:29 -0600

    ahhhhhhhhhhhhhhhhhhhhhhhhh

--- src/Prog214cCL.java
+++ src/Prog214cCL.java
@@ -1,7 +1,23 @@
+import java.util.*;
 public class Prog214cCL {
     private String type;
     private String Wash;
     private double galCost;
     private double Cost;
-    private double
+    private double g;
+
+    //rivate ArrayList<String> ALtype;
+    //private ArrayList<String> ALWash;
+    //private ArrayList<Double> ALGal;
+
+public class Prog213cCL(String typers, String washers, double gallons){
+    type = typers;
+    Wash = washers;
+    galCost = gallons;
+}
+
+
+
+
+
 }
--- src/Prog241c.java
+++ src/Prog241c.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Prog241c {
@@ -22,6 +23,32 @@ public class Prog241c {
      */
         try {  // On Replit, may need to put "../data/prog285b.dat"
             Scanner input = new Scanner(new File("data/prog214c.dat"));
+
+            ArrayList<String> type = new ArrayList<String>();
+            ArrayList<String> wash = new ArrayList<String>();
+            ArrayList<Double> gal = new ArrayList<Double>();
+            ArrayList<Integer> count = new ArrayList<Integer>();
+
+
+            while(input.hasNext()) {
+                for(int lcv = 0;){ //FIX THIS
+                    type.add(input.nextLine());
+                    gal.add(input.nextDouble());
+                    wash.add(input.nextLine());
+                    count.add(lcv);
+
+                }
+            }
+            while(input.hasNext()) {
+                for(int x : count)
+                Prog214cCL wow = new Prog214cCL(type.get(x), wash.get(x), gal.get(x));
+
+
+
+
+
+            }
+
 
 
         } catch (IOException e) {
