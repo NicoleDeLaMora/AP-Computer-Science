08db77a3f10e71605af56e21c35f664b4c349efe
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2022-12-15 10:31:10 -0600

    jhadskgjhag

--- src/Prog214cCL.java
+++ src/Prog214cCL.java
@@ -4,18 +4,50 @@ public class Prog214cCL {
     private String Wash;
     private double galCost;
     private double Cost;
-    private double g;
+    private double gal;
 
     //rivate ArrayList<String> ALtype;
     //private ArrayList<String> ALWash;
     //private ArrayList<Double> ALGal;
-/*
+
 public class Prog213cCL(String typers, String washers, double gallons){
-    type = typers;
-    Wash = washers;
-    galCost = gallons;
+
+        type = typers;
+        Wash = washers;
+        gal = gallons;
+
 }
-*/
+public static double CalcGas(){
+    return type * gal;
+}
+public static double CalcWash(){
+    if(Wash.equals("Y")){
+
+        if(gal >= 20){
+            return 0.0;
+        }else if()
+
+
+    }else return 0.0;
+}
+
+
+    /*
+    A service station with a car wash sells three grades of gasoline:
+    regular unleaded at $1.359 per gallon,
+    premium at $1.479 per gallon,
+    and high octane at $1.429 per gallon
+
+    A car wash normally costs $2.00, but the station offers a 10% reduction on the price of a
+    wash for every full gallon of gasoline purchased in the excess of the first 10 gallons.
+    When 20 or more gallons are purchased, the car wash is free
+
+
+      The program should print the cost of the gasoline and the price of a wash,
+      read another character indicates whether or not the customer wants a car wash,
+      and then print the total charges in the form of an invoice
+
+     */
 
 
 
