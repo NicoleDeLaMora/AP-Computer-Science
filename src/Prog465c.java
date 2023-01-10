8502f6dbe933623ca734f1f0c5c391f5aeb3249c
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2023-01-10 10:27:09 -0600

    465c

--- src/Prog465c.java
+++ src/Prog465c.java
@@ -5,18 +5,37 @@ import java.util.*;
 public class Prog465c {
     public static void main(String[] args){
         try {  // On Replit, may need to put "../data/prog285b.dat"
-            Scanner input = new Scanner(new File("data/prog285b.dat"));
-            int c = input.nextInt();
+            Scanner input = new Scanner(new File("data/prog465c.txt"));
+
             int r = input.nextInt();
+            int c = input.nextInt();
             int[][] list = new int[r][c];
             for(int row = 0; row < list.length; row++){
-                for(int col = 0; col < list.length; col++){
+                for(int col = 0; col < list.length+1; col++){
                     list[row][col] = input.nextInt();
                 }
             }
-
-
-
+            for(int[] row : list){
+                for(int n : row){
+                    System.out.print(n + " ");
+                }
+                System.out.println();
+            }
+            int count = 0;
+            for(int ro = 0; ro < list.length; ro++){
+                for(int co = 0; co < list.length; co++){
+                    if(ro == 0){
+                       count += list[ro][co];
+                    }else if(co == c-1){
+                        count += list[ro][co];
+                    }else if(ro == r - 1){
+                        count += list[ro][co];
+                    }else if(co == 0){
+                        count+= list[ro][co];
+                    }
+                }
+            }
+            System.out.println("The total of the edge is: " + count);
 
         } catch (IOException e) {
             System.out.println("Can't find data file!");
