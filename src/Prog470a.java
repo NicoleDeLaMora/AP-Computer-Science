8a924d65b7a5c8fdec055046604909228c9b0697
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2023-01-11 09:59:33 -0600

    completed 465c

---
+++ src/Prog470a.java
@@ -0,0 +1,16 @@
+//Nicole de la Mora // 01/11/23 // Prog470a
+import java.util.*;
+import java.io.*;
+public class Prog470a {
+    public static void main(String[] args){
+        try {  // On Replit, may need to put "../data/prog285b.dat"
+            Scanner input = new Scanner(new File("data/prog464a.dat"));
+
+
+
+
+        } catch (IOException e) {
+            System.out.println("Can't find data file!");
+        }
+    }
+}
