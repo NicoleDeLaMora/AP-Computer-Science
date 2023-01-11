df5e8c6647340761cafff1e26f08e7da167b499a
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2023-01-11 10:17:49 -0600

    almost done wit 470a

--- src/Prog470a.java
+++ src/Prog470a.java
@@ -5,9 +5,27 @@ public class Prog470a {
     public static void main(String[] args){
         try {  // On Replit, may need to put "../data/prog285b.dat"
             Scanner input = new Scanner(new File("data/prog464a.dat"));
+            int[][] list = new int[5][6];
+            for(int row = 0; row < list.length; row++){
+                for(int col = 0; col < list[0].length; col++){
+                    list[row][col] = input.nextInt();
+                }
+            }
 
+            for(int c = 0; c < list.length; c++){
+                int colCount = 0;
+                for(int r = 0; r < list[0].length; r++){
+                    colCount+= list[r][c];
+                }
+                list[4][c] = colCount;
+            }
 
-
+            for (int[] row : list) {
+                for (int num : row) {
+                    System.out.print(num + " ");
+                }
+                System.out.println();
+            }
 
         } catch (IOException e) {
             System.out.println("Can't find data file!");
