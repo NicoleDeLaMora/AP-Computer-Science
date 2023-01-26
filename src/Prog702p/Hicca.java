15bb8c3faac903e74acb319cda135d134ba2333f
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2023-01-26 10:29:27 -0600

    cried

--- src/Prog702p/Hicca.java
+++ src/Prog702p/Hicca.java
@@ -1,5 +1,12 @@
 package Prog702p;
 
 public class Hicca extends Animal{
-    private
+    private double myFur;
+    private String myName;
+    private String myWord;
+
+    public Hicca(String name, String word, double cost){
+        super(name, word);
+
+    }
 }
