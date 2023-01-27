5cb6e58b9ecb4e8fe15ac51026e781f8c161ed4c
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2023-01-27 10:30:31 -0600

    cried

--- src/Prog702p/Hicca.java
+++ src/Prog702p/Hicca.java
@@ -2,11 +2,12 @@ package Prog702p;
 
 public class Hicca extends Animal{
     private double myFur;
-    private String myName;
-    private String myWord;
+
 
     public Hicca(String name, String word, double cost){
         super(name, word);
-
+        myFur = cost;
     }
+
+    public double getFurCost(){return myFur;}
 }
--- src/Prog702p/Walleye.java
+++ src/Prog702p/Walleye.java
@@ -1,4 +1,12 @@
 package Prog702p;
 
-public class Walleye {
+public class Walleye extends Animal{
+    private int mySteps;
+
+    public Walleye(String name, String word, int s){
+        super(name, word);
+        mySteps = s;
+    }
+
+    public int getSteps(){return mySteps;}
 }
