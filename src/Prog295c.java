3dbe464db5bdd70e94e1d00946c30d8b92c19bac
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2023-02-17 10:29:39 -0600

    kms

---
+++ src/Prog295c.java
@@ -0,0 +1,18 @@
+//Nicole de la Mora // 02/17/23 // Prog295c
+import java.util.*;
+import java.io.*;
+public class Prog295c {
+    public static void main(String[] args){
+        try {  // On Replit, may need to put "../data/prog285b.dat"
+            Scanner in1 = new Scanner(new File("data/merge1.dat"));
+            Scanner in2 = new Scanner(new File("data/merge3.dat"));
+
+
+
+
+        } catch (IOException e) {
+            System.out.println("Can't find data file!");
+        }
+    }
+
+}
