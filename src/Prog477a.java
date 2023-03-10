a941f694e1a8b6b7e2d07899386923a170b855a7
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2023-03-10 10:28:45 -0600

    477a almost

--- src/Prog477a.java
+++ src/Prog477a.java
@@ -8,38 +8,52 @@ public class Prog477a {
             Scanner input = new Scanner(new File("data/prog477a.dat"));
             int[][] list = new int[5][3];
 
+            while(input.hasNext()){
+                if(input.nextInt() == 1) {
+                    int num = input.nextInt();
+                    if (num == 1)
+                        list[0][0]++;
+                    else if (num == 2)
+                        list[1][0]++;
+                    else if (num == 3)
+                        list[2][0]++;
+                    else if (num == 4)
+                        list[3][0]++;
+                    else if (num == 5)
+                        list[4][0]++;
+                }
+                else {
+                    int num = input.nextInt();
+                    if (num == 1)
+                        list[0][1]++;
+                    else if (num == 2)
+                        list[1][1]++;
+                    else if (num == 3)
+                        list[2][1]++;
+                    else if (input.nextInt() == 4)
+                        list[3][1]++;
+                    else if (input.nextInt() == 5)
+                        list[4][1]++;
+
+                }
+
+            }
+            }
 
             for(int row = 0; row < list.length; row++){
-                for(int col = 0; col < list.length; col++){
-                    if(input.nextInt() == 1){
-                        if(input.nextInt() == 1)
-                            list[0][0]++;
-                        if(input.nextInt() == 2)
-                            list[1][0]++;
-                        if(input.nextInt() == 3)
-                            list[2][0]++;
-                        if(input.nextInt() == 4)
-                            list[3][0]++;
-                        if(input.nextInt() == 5)
-                            list[4][0]++;
-                    else
-                        if(input.nextInt() == 1)
-                            list[0][1]++;
-                        if(input.nextInt() == 2)
-                            list[1][1]++;
-                        if(input.nextInt() == 3)
-                            list[2][1]++;
-                        if(input.nextInt() == 4)
-                            list[3][1]++;
-                        if(input.nextInt() == 5)
-                            list[4][1]++;
-                    }
+                for(int col = 0; col < list[0].length; col++){
+                    if(col == 2)
+                        list[row][2] = list[row][1] + list[row][0];
+
                 }
             }
 
-
-
-
+            System.out.print("Grade\tMale\tFemale\tTotal\n");
+            System.out.println("A\t" + list[0][0] + "\t" + list[0][1] + "\t" + list[0][2]);
+            System.out.println("B\t" + list[1][0] + "\t" + list[1][1] + "\t" + list[1][2]);
+            System.out.println("C\t" + list[2][0] + "\t" + list[2][1] + "\t" + list[2][2]);
+            System.out.println("D\t" + list[3][0] + "\t" + list[3][1] + "\t" + list[3][2]);
+            System.out.println("F\t" + list[4][0] + "\t" + list[4][1] + "\t" + list[4][2]);
 //int row = grade
             //int col = gender (?)
             //list[row][col] ++
