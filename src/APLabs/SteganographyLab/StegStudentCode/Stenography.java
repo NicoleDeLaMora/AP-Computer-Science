ba0aea7ba0fc1393f0e5b2c635b0355631176c1e
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2023-04-18 10:30:14 -0500

    stenography lab progress -- not fast enough though

---
+++ src/APLabs/SteganographyLab/StegStudentCode/Stenography.java
@@ -0,0 +1,14 @@
+package APLabs.SteganographyLab.StegStudentCode;
+import java.awt.Color;
+public class Stenography {
+    //Clear the lower rightmost two bits in a pixel
+    public static void clearLow(Pixel p){
+
+    }
+    public static void main(String[] args){
+
+    }
+    public static Picture testClearLow(Picture q){
+
+    }
+}
