8a910a03db71d22bca7967cf0c9eb8180144d513
Nicole de la Mora <delamora.n@stu.janesville.k12.wi.us>
2023-05-19 10:30:48 -0500

    41:30 in vid

--- src/Prog7300P/Perceptron.java
+++ src/Prog7300P/Perceptron.java
@@ -29,4 +29,29 @@ public class Perceptron {
         return activation.apply(dot_product);
     }
 
+    public double[] predict(double[][] X_test){
+        double[] perceptions = new double[X_test.length];
+        for(int i = 0; i < X_test.length; i++){
+            perceptions[i] = forward(X_test[i]);
+        }
+        return perceptions;
+    }
+    public void fir(double[][] X_train, double[] y_train){
+        weights = new double[X_train[0].length];
+        for(int i = 0; i < weights.length; i++){
+            weights[i] = Math.random()-0.5;
+        }
+        int n_samples = X_train.length;
+        for(int epoch = 0; epoch < epochs; epoch++){
+            double less = 0;
+            for(int i = 0; i < n_samples; i++){
+                double y_pred = forward(X_train[i]);
+                double error = y_train[i]-y_pred;
+                less += Math.pow(error,2);
+
+
+            }
+        }
+    }
+
 }
