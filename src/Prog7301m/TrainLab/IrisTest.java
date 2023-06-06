7c0a661e6d0e8eaefb00f843e1602a719c4df584
NicoleDeLaMora <112962823+NicoleDeLaMora@users.noreply.github.com>
2023-06-06 00:39:39 +0000

    l

---
+++ src/Prog7301m/TrainLab/IrisTest.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.io.*;
+public class IrisTest {
+public static int argmax(double[] a) {
+  int max = 0;
+  for (int i = 1; i < a.length; i++)
+    if (a[i] > a[max]) max = i;
+  return max;
+}
+public static void main(String[] args) {
+  try {
+  // Training Data
+    String[] labels = { "Iris-setosa", "Iris-versicolor", "Iris-        virginica" };
+    Scanner input = new Scanner(new File("data/IrisLab.csv"));
+    ArrayList<Double[]> Xtr = new ArrayList<Double[]>();
+    ArrayList<Double[]> ytr = new ArrayList<Double[]>();
+    input.nextLine();
+    while (input.hasNextLine()) {
+      String[] line = input.nextLine().split(",");
+      Double[] row = new Double[line.length - 2];
+      Double[] label_onehot = new Double[3];
+      int label = Arrays.asList(labels).indexOf(line[line.length-1]);
+      for (int i = 0; i < label_onehot.length; i++)
+        label_onehot[i] = 0.0;
+      label_onehot[label] = 1.0;
+      for (int i = 1; i < line.length-1; i++)
+        row[i - 1] = Double.parseDouble(line[i]);
+      Xtr.add(row);
+      ytr.add(label_onehot);
+}
+  int train_size = (int)(Xtr.size() * 0.8);
+  int test_size = Xtr.size() - train_size;
+  double[][] X_train = new double[train_size][Xtr.get(0).length];
+  double[][] y_train = new double[train_size][ytr.get(0).length];
+  for (int i = 0; i < train_size; i++) {
+    for (int j = 0; j < Xtr.get(0).length; j++)
+      X_train[i][j] = Xtr.get(i)[j];
+    for (int j = 0; j < ytr.get(0).length; j++)
+      y_train[i][j] = ytr.get(i)[j];
+  }
+  double[][] X_test = new double[test_size][Xtr.get(0).length];
+  double[][] y_test = new double[test_size][ytr.get(0).length];
+  for (int i = 0; i < test_size; i++) {
+  for (int j = 0; j < Xtr.get(0).length; j++)
+    X_test[i][j] = Xtr.get(i + train_size)[j];
+  for (int j = 0; j < ytr.get(0).length; j++)
+    y_test[i][j] = ytr.get(i + train_size)[j];
+}
+// Create model
+  int inputs = X_train[0].length;
+  int outputs = y_train[0].length;
+// int hiddenNeurons = 10; // Also try (int)Math.sqrt(inputs*outputs);
+  int hiddenL1 = outputs * 6;
+  int hiddenL2 = outputs * 3;
+  var model = new MultiLayerPerceptron(new int[]{ inputs, hiddenL1,   hiddenL2, outputs }, 0.1, new Activations.ReLU());
+  model.train(X_train, y_train, 10);
+  System.out.println("Final Accuracy: " + model.accuracy(X_test,   y_test));
+// Pick a random sample from the test set
+  int index = (int) (Math.random() * X_test.length);
+  double[] X_val = X_test[index];
+  double[] y_val = y_test[index];
+// Predict the label
+  double[] y_pred = model.predict(X_val);
+  String pred = labels[argmax(y_pred)];
+  String actual = labels[argmax(y_val)];
+  System.out.println("Predicted: " + pred + "\tActual: " + actual);
+} catch (IOException e) {
+System.out.println("Cannot find file");
+}
+}
+}
