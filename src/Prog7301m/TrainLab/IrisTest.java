8e4e83e4265767acccc9852e8162ebc0e954f4b3
NicoleDeLaMora <112962823+NicoleDeLaMora@users.noreply.github.com>
2023-06-07 00:01:06 +0000

    Cant run AI Lab but its coded

--- src/Prog7301m/TrainLab/IrisTest.java
+++ src/Prog7301m/TrainLab/IrisTest.java
@@ -1,5 +1,10 @@
+package Prog7301m.TrainLab;
+
+import Prog7301m.Activations;
+import Prog7301m.MultiLayerPerceptron;
 import java.util.*;
 import java.io.*;
+
 public class IrisTest {
 public static int argmax(double[] a) {
   int max = 0;
@@ -10,21 +15,27 @@ public static int argmax(double[] a) {
 public static void main(String[] args) {
   try {
   // Training Data
-    String[] labels = { "Iris-setosa", "Iris-versicolor", "Iris-        virginica" };
-    Scanner input = new Scanner(new File("data/IrisLab.csv"));
+    String[] labels = { "rank	points",	"goalsDiff",	"team_id",	"team.name",	"matches_played",	"wins",	"draws",	"losses",	"goals_for",	"goals_against",	"home",	"home_wins",	"home_draw",	"home_lose",	"home_goals_for", "home_goals_against",	"away",	"away_wins",	"away_draw",	"away_lose",	"away_goals_for",	"away_goals_against" };
+    Scanner input = new Scanner(new File("data/2023_PL_standings.csv"));
     ArrayList<Double[]> Xtr = new ArrayList<Double[]>();
     ArrayList<Double[]> ytr = new ArrayList<Double[]>();
     input.nextLine();
     while (input.hasNextLine()) {
       String[] line = input.nextLine().split(",");
       Double[] row = new Double[line.length - 2];
-      Double[] label_onehot = new Double[3];
-      int label = Arrays.asList(labels).indexOf(line[line.length-1]);
-      for (int i = 0; i < label_onehot.length; i++)
+      Double[] label_onehot = new Double[20];
+      int label = Arrays.asList(labels).indexOf(line[2]);
+      for (int i = 0; i < label_onehot.length; i++){
         label_onehot[i] = 0.0;
+      }
+        
       label_onehot[label] = 1.0;
-      for (int i = 1; i < line.length-1; i++)
-        row[i - 1] = Double.parseDouble(line[i]);
+      label_onehot[label+1] = 1.0;
+      for (int i = 0; i < line.length; i++){
+        if(i != 3 || i != 4){
+           row[i - 1] = Double.parseDouble(line[i]);
+        }
+      }
       Xtr.add(row);
       ytr.add(label_onehot);
 }
@@ -52,7 +63,7 @@ public static void main(String[] args) {
 // int hiddenNeurons = 10; // Also try (int)Math.sqrt(inputs*outputs);
   int hiddenL1 = outputs * 6;
   int hiddenL2 = outputs * 3;
-  var model = new MultiLayerPerceptron(new int[]{ inputs, hiddenL1,   hiddenL2, outputs }, 0.1, new Activations.ReLU());
+  var model = new MultiLayerPerceptron(new int[]{ inputs, hiddenL1,   hiddenL2, outputs }, 0.01, new Activations.Sigmoid());
   model.train(X_train, y_train, 10);
   System.out.println("Final Accuracy: " + model.accuracy(X_test,   y_test));
 // Pick a random sample from the test set
@@ -69,3 +80,6 @@ System.out.println("Cannot find file");
 }
 }
 }
+/*
+Cant run it but its coded
+ */
